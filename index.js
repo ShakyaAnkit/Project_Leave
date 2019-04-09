03082cf7864d104ec24ec2b0afe548eb58e5c803
ShakyaAnkit <shakyaankitt001@gmail.com>
2019-04-09 16:51:47 +0530

    add all files

---
+++ index.js
@@ -0,0 +1,11 @@
+//performing a task
+function greet(name,lastName){
+    console.log('hello '+ name+' '+lastName);
+}
+greet('john','doe');
+greet('Mary');
+//calculate a value
+funtion square(number){
+    return number*number;
+}
+console.log(square(2));
